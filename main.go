500e6779d1fb8747cfa9333c70f21f91b5b96189
ithallojunior <ithallojunior@outlook.com>
2021-07-08 22:49:32 -0300

    feat: add working day type

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+
+type WorkingDay struct {
+	Success bool
+
+	ClockIn    string
+	StartBreak string
+	EndBreak   string
+	ClockOut   string
+	Total      string
+}
 func main() {
 
 }
